32cd09241cbec87ee9ab901ee6d5fbdf1f038219
daangemist <git@daangemist.nl>
2022-10-09 00:59:26 +0200

    Trim the baseUrl provided in the prefix, so it never ends in a forward slash.

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,15 @@ import * as requests from './requests';
 export async function initialize(options: Options): Promise<Client> {
   const requester = await getRequester(options);
 
+  const rawBaseUrl = options.baseUrl;
+  const baseUrl =
+    rawBaseUrl.charAt(rawBaseUrl.length) === '/'
+      ? rawBaseUrl.slice(0, Math.max(0, rawBaseUrl.length - 1))
+      : rawBaseUrl;
+
   return {
-    login: requests.login(options.baseUrl, requester),
-    register: requests.register(options.baseUrl, requester),
-    refresh: requests.refresh(options.baseUrl, requester),
+    login: requests.login(baseUrl, requester),
+    register: requests.register(baseUrl, requester),
+    refresh: requests.refresh(baseUrl, requester),
   };
 }
